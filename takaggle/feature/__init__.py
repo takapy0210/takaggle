2b35616aacd14c23eac918bf46a7f4d9401546cc
takapy <takanobu.030210@gmail.com>
2020-08-23 13:12:47 +0900

    add init

--- takaggle/feature/__init__.py
+++ takaggle/feature/__init__.py
@@ -2,3 +2,4 @@ from takaggle.feature.bert_sentence_vectorizer import *
 from takaggle.feature.category_encoder import *
 from takaggle.feature.engneering import *
 from takaggle.feature.selection import *
+from takaggle.feature.reduce_memory import *
---
+++ takaggle/tools/__init__.py
@@ -0,0 +1,6 @@
+from takaggle.tools.base import *
+from takaggle.tools.create_pickle_data import *
+from takaggle.tools.slack_notify import *
+from takaggle.tools.timer import *
+from takaggle.tools.submit_kaggle import *
+from takaggle.tools.slack_notify import *
