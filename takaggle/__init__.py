03401b00094cd6d568003c48b79302c306ce790f
takapy <takanobu.030210@gmail.com>
2020-08-23 10:50:41 +0900

    mod init

--- takaggle/__init__.py
+++ takaggle/__init__.py
@@ -1,7 +1,7 @@
-from takaggle.eda import *
-from takaggle.feature import *
-from takaggle.tools import *
-from takaggle.training import *
+from takaggle.takaggle.eda import *
+from takaggle.takaggle.feature import *
+from takaggle.takaggle.tools import *
+from takaggle.takaggle.training import *
 
 
 def main():
