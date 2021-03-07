748a8524db7302df328e73ad30bd326d3bbf0da6
takapy <takanobu.030210@gmail.com>
2021-03-07 14:16:49 +0900

    Release 2021/03/07 14:16:49

--- takaggle/feature/__init__.py
+++ takaggle/feature/__init__.py
@@ -1,5 +1,5 @@
 from takaggle.feature.bert_sentence_vectorizer import *
 from takaggle.feature.category_encoder import *
-from takaggle.feature.engneering import *
-from takaggle.feature.selection import *
+from takaggle.feature.feature_engineering import *
+from takaggle.feature.feature_selection import *
 from takaggle.feature.reduce_memory import *
